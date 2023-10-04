4f9d8196de519c75e9115d67314b86c9699fa234
Jennynny <101732141+Jennynny@users.noreply.github.com>
2023-10-04 15:16:03 +0300

    alcometer

--- App.js
+++ App.js
@@ -1,20 +1,114 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, Pressable, Switch } from 'react-native';
+import {lightStyle, darkStyle,numInputs, radioStyle,promilles} from './styles/Styles.js';
+import { useState } from 'react';
+import NumericInput from 'react-native-numeric-input';
+import { RadioButton } from 'react-native-paper';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [dark, setDark] = useState(false); 
+  let myStyle = dark ? darkStyle : lightStyle;
+  const [value, setValue] = useState(0);
+  //const [radioval, setRadioval] = useState('');
+  const [gender, setGender] = useState('m');
+  const [bottles, setBotless] = useState('');
+  const [time, setTime] = useState('');
+  const [promilles, setPromilles] = useState('');
+
+  const [weight, setWeight] = useState(0);
+
+  //console.log(weight)
+
+  const count =() => {
+    
+   // e.preventDefault()
+    let result = 0
+
+    let litres = bottles * 0.33
+    let grams = litres * 8 * 4.5
+    let burning = weight / 10
+    let gramsLeft = grams - (burning * time)
+    if (!weight){
+      alert('Set weight!')
+    }else{
+      if (gender === 'm'){
+        result = gramsLeft/(weight * 0.7) 
+     
+      }else{
+        result = gramsLeft/(weight * 0.6)
+      
+      } 
+      if (result <0){
+        result = 0
+      }
+
+      setPromilles(result.toFixed(2))
+  }}
+
+  return (
+    <View style={myStyle.container}>
+      <View style={myStyle.swi}>
+        <Switch 
+        value={dark}
+        onValueChange={ newValue => setDark(newValue)}
+        />
+      </View>
+      <Text style={myStyle.header}>Alcometer</Text>
+      <Text style={myStyle.label}>Weight</Text>
+      <TextInput keyboardType='number-pad' style={myStyle.textInput}
+        onChangeText={(v) => setWeight(v)}
+        value={weight}      
+      />
+      <Text style={myStyle.label}>Bottles</Text>
+      <View style={myStyle.numInputs}>
+        <NumericInput onChange={v => setBotless(v)}
+          totalWidth={135}
+          value = {bottles}
+          minValue={0}
+          rounded
+          containerStyle={{backgroundColor: '#ffffff'}} 
+          color='#000000'
+          textColor='#000000'
+          rightButtonBackgroundColor = '#0ad7de'
+          leftButtonBackgroundColor= '#0ad7de'
+          iconStyle={{color:'#000000'}}
+          borderColor='#07b0f3'
+        />
+      </View>
+      <Text style={myStyle.label}>Hours</Text>
+      <View style={myStyle.numInputs}>
+        <NumericInput onChange={v => setTime(v)}
+        totalWidth={135}
+        value={time}
+        minValue={0}
+        rounded
+        />
+      </View>
+      <RadioButton.Group onValueChange={newValue => setGender(newValue)} value={gender}>
+        <View style={myStyle.radioStyle}>
+          <RadioButton value= 'm'
+          color='#ffffff'
+        />
+        <Text style={myStyle.radioStyle}>Male</Text>
+        </View>
+        <View style ={myStyle.radioStyle}>
+          <RadioButton value='f'
+            color='#ffffff'
+          />
+        <Text style={myStyle.radioStyle}>Female</Text>
+        </View>
+      </RadioButton.Group>
+      <View style={myStyle.promillesStyles}>
+        <Text style={myStyle.prom}>
+          {promilles} 
+        </Text>
+
+      </View>
+      <TouchableOpacity onPress={()=>count()}>
+          <Text style={myStyle.submit}>CALCULATE</Text>
+        </TouchableOpacity>
+    </View>
+    
+  );
+};
---
+++ styles/Styles.js
@@ -0,0 +1,154 @@
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+//export default StyleSheet.create({
+const lightStyle = StyleSheet.create({
+container:{
+    marginTop: Constants.statusBarHeight+5,
+    backgroundColor: '#c65fef',
+    flex: 1
+},
+swi:{
+    flexDirection: 'row',
+   // alignItems: '',
+    justifyContent:'flex-start',
+    padding: 15
+},
+header: {
+    color: '#000000',
+    textAlign: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 35
+},
+label: {
+    color: '#ffffff',
+    fontSize: 20,
+    paddingLeft: 15,
+},
+textInput:{
+    //paddingLeaft: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+    marginTop: 5,
+    borderColor: '#ffffff',
+    color:'#ffffff'
+},
+submit:{
+    borderWidth: 1,
+    borderRadius: 5,
+    color: 'white',
+    backgroundColor: '#290ff0',
+    padding: 15,
+    margin: 10,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold'
+},
+numInputs:{
+    paddingLeft: 25,
+    //justifyContent: "center",
+    //alignItems: "start",
+    //gap: 10
+   
+},
+radioStyle:{
+    paddingLeft: 15,
+   // paddingTop: 5,
+    flexDirection: 'row', 
+     alignItems:'center',
+     color:'#ffffff',
+     fontWeight:'bold',
+     fontSize: 18,
+     
+},
+promillesStyles: {
+    alignItems:'center',
+   
+   
+}, 
+prom:{
+    color:'#000000',
+    fontSize: 38,
+    fontWeight: 'bold'
+}
+
+
+});
+
+const darkStyle = StyleSheet.create({
+    container: {
+    ...lightStyle.container,
+    backgroundColor: '#470476'
+    },
+    swi: {
+    ...lightStyle.swi
+    },
+    header: {
+    ...lightStyle.header,
+    color: '#ffffff'
+    },
+    label: {
+        ...lightStyle.label
+    },
+    textInput: {
+        ...lightStyle.textInput
+    },
+    submit: {
+    ...lightStyle.submit
+    },
+    numInputs:{
+        ...lightStyle.numInputs
+    },
+    radioStyle:{
+        ...lightStyle.radioStyle
+    },
+    promillesStyles: {
+        ...lightStyle.promillesStyles
+       
+       
+    }, 
+    prom:{
+        ...lightStyle.prom,
+        color:'#ffffff'
+    }
+});
+
+// const numInputs = StyleSheet.create({
+//     paddingLeft: 25,
+//     //justifyContent: "center",
+//     alignItems: "start",
+//     //gap: 10
+//     //textColor:'#12c315',
+//     //rightButtonBackgroundColor: '#000000',
+//     //leftButtonBackgroundColor: '#0000FF',
+//     //iconStyle:{color:'#ab0f0f'},
+//     //borderColor:'#FFFF00'
+//     color:'#000000',
+//     textColor:'#8feb9a',
+//     containerStyle:{backgroundColor: '#9e0808'},
+//     rightButtonBackgroundColor : '#5b498b',
+//     leftButtonBackgroundColor: '#0000FF',
+//     iconStyle:{color:'#f4e21b'},
+//     borderColor:'#7bd2f5'
+// })
+
+// const radioStyle= StyleSheet.create({
+//     flexDirection: 'row', 
+//     alignItems:'center',
+//     color:'#ffffff'
+
+// });
+
+//const promilles = StyleSheet.create({
+  //  color:'#ffffff',
+    //fontWeight:'bold',
+    //fontSize: 38,
+//})
+
+export {lightStyle, darkStyle};
+//numInputs,
+//,radioStyle
+//,promilles
